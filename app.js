1c40b4b2379b245aabaaa94e3bb803e53c876f45
git status <tifchang@wharton.upenn.edu>
2017-07-17 12:18:45 -0700

    server

--- app.js
+++ app.js
@@ -13,7 +13,20 @@ app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
 app.post('/hello', function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+    text : 'Hello ' + userName.toUpperCase() + ', welcome to TestMyBotHorizons Slack channel! I\'ll be your guide bitches!'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
+app.post('/login', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName.toUpperCase() + ', welcome to TestMyBotHorizons Slack channel! I\'ll be your guide bitches!'
   };
   // Loop otherwise..
   if (userName !== 'slackbot') {
