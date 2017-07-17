daaa124663d47a3ace68119a92417727ccd19bfc
git status <tifchang@wharton.upenn.edu>
2017-07-17 11:56:18 -0700

    heroku

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
+
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
