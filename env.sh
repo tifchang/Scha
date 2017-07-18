fc640c09aec3c0954d832581ddc787d680fa68cf
Tiffany <tifchang@wharton.upenn.edu>
2017-07-18 11:28:52 -0700

    Delete env.sh

--- env.sh
+++
@@ -1,2 +0,0 @@
-export MONGODB_URI="mongodb://tiff:hi@ds163672.mlab.com:63672/sachathescheduler"
-export CLIENT_SECRET='{"web":{"client_id":"395858840719-jc7r3lsbdjirl1u0o3l0doo5chhsntmi.apps.googleusercontent.com","project_id":"tidy-scholar-174021","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"ThEz4xu-YPaWQdRzbYKtBsyT","redirect_uris":["https://mighty-ravine-89622.herokuapp.com/"]}}'
