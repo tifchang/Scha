855d4b0d8f3880741beec72a26933bf9d9a00836
crestwood204 <andong@sas.upenn.edu>
2017-07-19 11:00:50 -0700

    removed reminder functions from bot.js

--- bot.js
+++ bot.js
@@ -13,55 +13,8 @@ var User = Models.User;
 var Task = Models.Task;
 var Meeting = Models.Meeting;
 
-function remindOneDayBefore() {
-  Task.find({})
-  .populate('requesterId')
-  .exec()
-  .then(function(tasks) {
-    var tomorrow = new Date();
-    tomorrow.setDate(new Date().getDate() + 1);
-    tomorrowString = tomorrow.toDateString();
-
-    tasks.forEach(function(task) {
-      //task.day can just be new Date();
-      if (task.day.toDateString() === tomorrowString) {
-        rtm.sendMessage(
-          `Hi ${task.requesterId.slackUsername}!
-           You have an event tomorrow: ${task.subject}
-          `, task.requesterId.slackDmId
-        )
-      }
-    });
-  })
-}
-
-function remindToday() {
-  Task.find()
-  .populate('requesterId')
-  .exec()
-  .then(function(tasks) {
-    var today = new Date();
-    todayString = today.toDateString();
-
-    tasks.forEach(function(task) {
-      if (task.day.toDateString() === todayString) {
-        rtm.sendMessage(
-          `Hi ${task.requesterId.slackUsername}!
-           You have an event today: ${task.subject}
-          `, task.requesterId.slackDmId
-        )
-
-        //delete from mongo
-        Task.remove(task)
-      }
-    })
-  })
-}
-
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   console.log(rtmStartData.self.name);
-  remindToday();
-  remindOneDayBefore();
 })
 
 // let messageInProgess = false;
