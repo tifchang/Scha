b0ff9f4c36d099c3b078788d1ca26ef13680c7fb
git status <tifchang@wharton.upenn.edu>
2017-07-20 10:54:22 -0700

    tiff

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ var axios = require('axios');
 var { CLIENT_EVENTS, RTM_EVENTS, RtmClient, WebClient } = require('@slack/client');
 
 
-var bot_token = 'xoxb-214569167297-MS1iiNvvMRhbbGCTjg1zqXzd';
+var bot_token = 'xoxb-213951919538-bJriEM0KyuH5n8DTzVXelHVO';
+
 
 var rtm = new RtmClient(bot_token);
 
@@ -11,14 +12,23 @@ var web = new WebClient(bot_token);
 
 var Models = require('./models/models');
 var User = Models.User;
+
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+    console.log(rtmStartData.self.name);
+})
+
+let messageInProgess = false;
+
+rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
 var Task = Models.Task;
 var Meeting = Models.Meeting;
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-  console.log(rtmStartData.self.name);
-})
+    console.log(rtmStartData.self.name);
 
-let messageInProgess = false;
+
+var toStore = {};
 
 rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
     //when yes or no is clicked reset messageInProgess to be false
@@ -27,11 +37,6 @@ rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
         messageInProgess = false;
     }
 
-})
-
-var toStore = {};
-
-rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
   //ensure the bot will ignore the message if it is not sent via DM
   var dm = rtm.dataStore.getDMByUserId(msg.user);
   if (!dm || dm.id !== msg.channel || msg.type !== 'message') {
@@ -39,26 +44,87 @@ rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
   }
 
   //check that the user has gone through authentication
-  User.findOne({slackId: msg.user})
-  .then(function(user) {
-    if (!user) {
-      return new User({
-        slackId: msg.user,
-        slackDmId: msg.channel
-      }).save();
-    }
-    user.pendingRequest = '';
-    user.save()
-    return user;
-  })
-  .then(function(user){
-    if (!user.google) {
-      rtm.sendMessage(`Hello!
-        This is scheduler bot. In order to schedule things for you, I need
-        access to your google calendar,
-        Please visit http://a2c5ad38.ngrok.io/connect?user=${user._id} to setup Google Calendar`, msg.channel);
-        return;
-    }
+    User.findOne({slackId: msg.user})
+    .then(function(user) {
+        if (!user) {
+            return new User({
+                slackId: msg.user,
+                slackDmId: msg.channel
+            }).save();
+        }
+        return user;
+    })
+    .then(function(user){
+        if (!user.google) {
+            rtm.sendMessage(`Hello!
+                This is scheduler bot. In order to schedule things for you, I need
+                access to your google calendar.
+
+                Please visit http://d31adc8e.ngrok.io/connect?user=${user._id} to setup Google Calendar`, msg.channel);
+                return;
+            }
+
+            if (messageInProgess) {
+                rtm.sendMessage('Please complete previous request!', user.slackDmId);
+                return;
+            }
+            axios.get('https://api.api.ai/api/query', {
+                params: {
+                    v: 20150910,
+                    lang: 'en',
+                    timezone: '2017-07-17T14:01:03-0700',
+                    query: msg.text,
+                    sessionId: user.slackId
+                },
+                headers: {
+                    Authorization: 'Bearer 30cb48540bc14e0ab93dd24392ec801c'
+                }
+            })
+            .then((res) => {
+                // check that action is complete
+                if (res.data.result.actionIncomplete) {
+                    rtm.sendMessage(res.data.result.fulfillment.speech, user.slackDmId)
+                    return;
+                } else if (res.data.result.action === "remind.add") {
+                    user.pendingRequest = JSON.stringify(res.data.result);
+                    console.log(user.pendingRequest);
+                    user.save()
+                    .then(function(user) {
+                        web.chat.postMessage(msg.channel, '', {
+                            "attachments": [
+                                {
+                                    "fallback": "fallback",
+                                    "title": res.data.result.fulfillment.speech,
+                                    "callback_id": msg.user,
+                                    "color": "#3AA3E3",
+                                    "attachment_type": "default",
+                                    "actions": [
+                                        {
+                                            "name": "Yes",
+                                            "text": "Yes",
+                                            "type": "button",
+                                            "value": "good"
+                                        },
+                                        {
+                                            "name": "No",
+                                            "text": "No",
+                                            "type": "button",
+                                            "value": "bad"
+                                        }
+                                    ]
+                                }
+                            ]
+                        });
+                    })
+
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+        })
+    })
 
     if (user.pendingRequest && JSON.parse(user.pendingRequest).action) {
       rtm.sendMessage('Please complete previous request!', user.slackDmId);
@@ -154,6 +220,9 @@ rtm.on(RTM_EVENTS.MESSAGE, (msg) => {
             ]
           });
         })
+      } else {
+        rtm.sendMessage(res.data.result.fulfillment.speech, user.slackDmId)
+      }
     })
     .catch((err) => {
       console.log(err);
